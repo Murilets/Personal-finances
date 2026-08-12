1c056d08b5901220cff28b03ebd7c3d5829a57bc
Murilets <murilobertelli18@gmail.com>
2026-08-12 15:36:49 -0300

    feat: add new repository interfaces

---
+++ src/Domain/Interfaces/IExpenseRepository.cs
@@ -0,0 +1,13 @@
+namespace FinChat.Domain.Interfaces;
+
+using FinChat.Domain.Entities;
+
+public interface IExpenseRepository
+{
+    Task<Expense?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<Expense>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<List<Expense>> GetByCategoryIdAsync(Guid categoryId, CancellationToken cancellationToken = default);
+    Task AddAsync(Expense expense, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Expense expense, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Expense expense, CancellationToken cancellationToken = default);
+}
